7c1d8a026db227b5610a8f6bc8b8c669b5e64c9a
vanfindi <163383295+vanfindi@users.noreply.github.com>
2024-04-13 22:49:05 -0700

    Update face.js

--- face/face.js
+++ face/face.js
@@ -4,13 +4,18 @@ background('rgb(99, 231, 189)');
 }
 
 function draw() {
+
+stroke("#87672f")
+fill("#c4a164");
+ellipse(300, 250, 260, 260) //behindhair
   
 fill(238, 200, 185);
+stroke(196, 146, 126)
 strokeWeight(3);
-arc(185, 300, 80, 80, 0, PI+HALF_PI); //left ear
+arc(185, 300, 60, 75, 0, PI+HALF_PI); //left ear
 fill(238, 200, 185);
 strokeWeight(3);
-arc(415, 300, 80, 80, PI, TWO_PI+HALF_PI); //right ear
+arc(415, 300, 60, 75, PI, TWO_PI+HALF_PI); //right ear
 
 noFill();
 stroke(134, 119, 120)
@@ -21,20 +26,34 @@ noFill();
 stroke(134, 119, 120)
 strokeWeight(4);
 circle(430, 350, 30); //hoopL
+
+fill(238, 200, 185);
+stroke(166, 116, 96)
+rect(265, 435, 70, 200, 20, 15, 10, 5); //neck
   
 fill(238, 200, 185);
-  stroke(51);
+  stroke(196, 146, 126);
   strokeWeight(3);
-  ellipse( 300, 300, 250, 300); //head
+  ellipse( 300, 300, 240, 300); //head
 
 noFill();
   stroke("#87672f")
   strokeWeight(2); //left hair
   fill("#c4a164");
   bezier(300, 160, 100, 180, 250, 300, 160, 400);
+  
+beginShape(); 
+vertex(240, 150);
+vertex(300, 140);
+vertex(360, 150);
+vertex(400, 175);
+vertex(425, 230);
+vertex(330, 200);
+vertex(230, 176);
+endShape(); //bang
  
 
-stroke(51)
+stroke("#505432")
 strokeWeight(3);
 fill(240);
 ellipse(240, 300, 80, 40); //left eye
@@ -57,13 +76,19 @@ stroke(51)
 circle(280, 350, 1); //mole
 
 fill(220, 107, 125);
-arc(300, 390, 40, 30, 5.5, PI + QUARTER_PI, CHORD); //mouth
+stroke(143, 63, 76);
+arc(297, 390, 40, 30, 5.5, PI + QUARTER_PI, CHORD); //mouth
 
+fill(238, 200, 185);
+  stroke(166, 116, 96);
+  arc(285, 375, 25, 20, 0, PI);
+  arc(310, 375, 25, 20, 0, PI);
 
 fill(238, 200, 185);
 arc(290, 340, 30, 30, 0, QUARTER_PI); //nose
 
 fill(255, 246, 215);
+stroke(179, 162, 107)
 arc(300, 650, 350, 350, PI, TWO_PI+HALF_PI); //body
 
 }
